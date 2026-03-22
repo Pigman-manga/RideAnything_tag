cc39d5f2248ede5165d83f3dcb259850b4a76894
Pigman <46450474+Pigman-manga@users.noreply.github.com>
2026-03-22 14:22:26 +0300

    Use mob AI controls for carrot steering

--- src/main/java/xyz/telecter/rideanything/RideAnythingMod.java
+++ src/main/java/xyz/telecter/rideanything/RideAnythingMod.java
@@ -9,6 +9,7 @@ import net.fabricmc.fabric.api.event.player.UseEntityCallback;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.MovementType;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
 import net.minecraft.server.MinecraftServer;
@@ -23,6 +24,7 @@ public class RideAnythingMod implements ModInitializer {
 	private static final String RIDEABLE_TAG = "rideble";
 	private static final String RIDEABLE_CARROT_TAG = "rideble_carrot";
 	private static final double CARROT_CONTROL_SPEED_MULTIPLIER = 0.35D;
+	private static final double CARROT_LOOKAHEAD_DISTANCE = 2.5D;
 
 	@Override
 	public void onInitialize() {
@@ -74,6 +76,35 @@ public class RideAnythingMod implements ModInitializer {
 		vehicle.headYaw = yaw;
 
 		Vec3d riderInput = new Vec3d(player.sidewaysSpeed, 0.0D, player.forwardSpeed);
+		if (vehicle instanceof MobEntity mobEntity) {
+			steerMobVehicle(mobEntity, riderInput, yaw);
+			return;
+		}
+
+		steerGenericVehicle(vehicle, riderInput, yaw);
+	}
+
+	private void steerMobVehicle(MobEntity vehicle, Vec3d riderInput, float yaw) {
+		vehicle.getNavigation().stop();
+		vehicle.setTarget(null);
+
+		Vec3d velocity = vehicle.getVelocity();
+		if (riderInput.lengthSquared() < 1.0E-4D) {
+			vehicle.setForwardSpeed(0.0F);
+			vehicle.setVelocity(velocity.x * 0.5D, velocity.y, velocity.z * 0.5D);
+			vehicle.velocityDirty = true;
+			return;
+		}
+
+		Vec3d movementDirection = getMovementDirection(riderInput, yaw);
+		double speed = vehicle.getMovementSpeed() + CARROT_CONTROL_SPEED_MULTIPLIER;
+		Vec3d moveTarget = new Vec3d(vehicle.getX(), vehicle.getY(), vehicle.getZ()).add(movementDirection.multiply(CARROT_LOOKAHEAD_DISTANCE));
+		vehicle.getMoveControl().moveTo(moveTarget.x, vehicle.getY(), moveTarget.z, speed);
+		vehicle.setForwardSpeed((float) speed);
+		vehicle.velocityDirty = true;
+	}
+
+	private void steerGenericVehicle(LivingEntity vehicle, Vec3d riderInput, float yaw) {
 		Vec3d velocity = vehicle.getVelocity();
 		if (riderInput.lengthSquared() < 1.0E-4D) {
 			vehicle.setVelocity(velocity.x * 0.5D, velocity.y, velocity.z * 0.5D);
@@ -81,13 +112,16 @@ public class RideAnythingMod implements ModInitializer {
 			return;
 		}
 
-		Vec3d movement = new Vec3d(riderInput.x, 0.0D, riderInput.z)
-				.normalize()
-				.rotateY((float) Math.toRadians(-yaw))
+		Vec3d movement = getMovementDirection(riderInput, yaw)
 				.multiply(vehicle.getMovementSpeed() + CARROT_CONTROL_SPEED_MULTIPLIER);
-
 		vehicle.move(MovementType.SELF, movement);
 		vehicle.setVelocity(movement.x, velocity.y, movement.z);
 		vehicle.velocityDirty = true;
+	}
+
+	private Vec3d getMovementDirection(Vec3d riderInput, float yaw) {
+		return new Vec3d(riderInput.x, 0.0D, riderInput.z)
+				.normalize()
+				.rotateY((float) Math.toRadians(-yaw));
 	}
 }
